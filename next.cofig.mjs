59ed4dda66531d6b0d3a0a3b974b079fc2ed107d
adamahmed29623-bit <adamahmed29623@gmail.com>
2025-12-31 15:59:10 +0200

    Delete next.cofig.mjs

--- next.cofig.mjs
+++
@@ -1,7 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    /* هذا السطر هو السر: يخبر Vercel أن يسمح للمكتبة بالعمل داخل صفحات الأكاديمية */
-    transpilePackages: ['lucide-react'],
-};
-
-export default nextConfig;
